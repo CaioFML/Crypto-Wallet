569221d3a8b1a3f9967934a5501ac32d151e9d8e
Caio <caio.felipemotta@hotmail.com>
2019-06-28 15:44:58 -0300

    59. Fluxo MVC do CRUD (Show e Delete) + Filtros Rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
   def br_date(date)
-    date.strftime("%d/%m/%Y")
+    date.strftime('%d/%m/%Y')
+  end
+
+  def ambiente_rails
+    ambiente = Rails.env
+    if ambiente.development?
+      'Desenvolvimento'
+    elsif ambiente.production?
+      'Produção'
+    else
+      'Testes'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  # get '/inicio', to: 'welcome#index'
+
   resources :coins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/coins', to: 'coins#index'
 
   root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
